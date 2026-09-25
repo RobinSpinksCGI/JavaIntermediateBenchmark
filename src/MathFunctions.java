f1534eee134a04fae67716bdb6ae99f472a5c2ce
Robin Spinks <robin.spinks@cgi.com>
2026-09-25 15:28:11 +0100

    Add question 4 to MathFunctions.java

--- src/MathFunctions.java
+++ src/MathFunctions.java
@@ -1,3 +1,23 @@
+/*
+This question is assessing your ability to create static (inner) nested classes in Java.
+
+Create a class named MathFunctions that contains two static nested classes.
+
+Within the MathFunctions class:
+
+Define static nested classes called Division and Multiplication for each mathematical operation.
+Each nested static class should contain a method to perform its respective operation: divide for the Division class and multiply for the Multiplication class.
+Define a main method inside the MathFunctions class such that it:
+has two instances of the nested classes, Division and Multiplication
+calls the methods of the nested classes to perform mathematical operations
+Lastly, test your code by printing the values of the operations.
+
+Requirements and Assumptions:
+
+The nested class names must be Division and Multiplication.
+The methods within the nested classes must be divide and multiply.
+*/
+
 public class MathFunctions {
     public static class Division {
         public double divide(int d1, int d2) {
